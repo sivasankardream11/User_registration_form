941d9430f8c72abb60750e3e4969c7730afe0f92
sivasankardream11 <sivasankardream11@gmail.com>
2023-02-11 18:50:41 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+from app.models import *
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model=User
+        fields=['username','password','email']
+        widgets={'password':forms.PasswordInput}
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=profile
+        fields=['address','profile_pic']
